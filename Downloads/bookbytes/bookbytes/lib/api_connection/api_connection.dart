662972fbd479db8202c0e0c210b5185b185d9ece
a00830hk <hui.koh-2@student.manchester.ac.uk>
2023-12-31 00:15:47 +0000

    First

---
+++ Downloads/bookbytes/bookbytes/lib/api_connection/api_connection.dart
@@ -0,0 +1,6 @@
+class API {
+  static const hostConnect = "http://10.205.175.168/bookbytes_server";
+  static const hostConnectUser = "$hostConnect/api/user";
+
+  static const register = "$hostConnectUser/register.php";
+}
